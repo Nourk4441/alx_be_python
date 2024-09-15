3ab34ecfd51506e8e902c21e9bb5f7357e658307
Nour Khaled <100867406+Nourk4441@users.noreply.github.com>
2024-09-15 23:08:32 +0300

    added basic_operations file

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,8 @@
+n1=10
+n2=5
+sum=n1+n2
+sub=n1-n2
+prod=n1*n2
+print('Addition of', n1 ,'and', n2,' is', sum)
+print('Subtraction of ', n1 ,'and', n2,' is', sub)
+print('Multiplication of' , n1 ,'and', n2,' is', prod)
